afb79a4981e0d1142f87dea830d50273d6b5853f
Evin Round <earound@alumni.fullsail.edu>
2023-05-30 22:42:48 -0400

    []

---
+++ week 1/script.js
@@ -0,0 +1,45 @@
+//Question 1
+
+function question1 () {
+    console.log("Hello!");
+}
+
+question1();
+
+//Question 2
+
+function  question2(Name) {
+    console.log(`Hello ${name}!`;
+}
+
+question2("John");
+
+//Question 3
+
+let name = prompt("What is your name?");
+
+function question3(name){
+    console.log(`Hello ${name}!`);
+}
+question3();
+
+//Question 4
+
+function question4(x , y){
+    const sum = x * y;
+    console.log(sum);
+}
+
+question4(2, 3);
+
+//Question 5
+
+function question5(x, y){
+   function getNumbers(){
+   }
+   function calculateNumbers(){
+   }
+   function displayNumbers(){
+   }
+    
+}
