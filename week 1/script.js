88ce29ccf86affbae3f6737de4b4f287b78ec0d2
Evin Round <earound@alumni.fullsail.edu>
2023-06-01 22:41:26 -0400

    []

--- week 1/script.js
+++ week 1/script.js
@@ -1,6 +1,7 @@
 //Question 1
 
 function question1 () {
+    console.log("Question 1");
     console.log("Hello!");
 }
 
@@ -9,21 +10,28 @@ question1();
 //Question 2
 
 function  question2(Name) {
-    console.log(`Hello ${name}!`;
+    let name = Name;
+    console.log(`Question 2`);
+    console.log(`Hello ${name}!`);
 }
 
 question2("John");
 
 //Question 3
+console.log("Question 3");
+let name2;
+name2 = prompt("What is your name?");
 
-let name = prompt("What is your name?");
-
-function question3(name){
-    console.log(`Hello ${name}!`);
+function question3(){
+    console.log(`Hello ${name2}!`);
 }
 question3();
 
 //Question 4
+
+console.log("Question 4");
+let x;
+let y;
 
 function question4(x , y){
     const sum = x * y;
@@ -34,12 +42,27 @@ question4(2, 3);
 
 //Question 5
 
-function question5(x, y){
-   function getNumbers(){
-   }
-   function calculateNumbers(){
-   }
-   function displayNumbers(){
-   }
-    
+console.log("Question 5");
+
+function question5(){
+    let x;
+    let y;
+    function getNumbers(){
+        x = prompt("Enter a number");
+        y = prompt("Enter another number");
+        parseInt(x);
+        parseInt(y);
+    }
+    getNumbers();
+    function calculateNumbers(x, y){
+        return x*y;
+    }
+    let result = calculateNumbers(x, y);
+    function displayNumbers(){
+        console.log(`The sum of ${x} times ${y} is ${result}`);
+    }
+    displayNumbers();
 }
+question5();
+    
+
