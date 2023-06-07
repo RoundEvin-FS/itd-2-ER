ab8e3600a8daa30deaa904f03a07102c8561afcd
Evin Round <earound@alumni.fullsail.edu>
2023-06-06 20:20:27 -0400

    [finished]

--- week 1/RoundEvinNumberPicker_1/js/main.js
+++ week 1/RoundEvinNumberPicker_1/js/main.js
@@ -12,26 +12,29 @@
     function getRandom(){
         return Math.floor(Math.random( ) * 5 + 1);
         } 
-        function gameOver(){
-            alert("Game Over!");
-            let answer = prompt("Do you want to play again?");
-            if(answer == "yes" || answer == "y"){
-                playGame();
-            } else {
-        "Game Over!"
-    }}
+         function gameOver(){
+                alert("Game Over!");
+                let answer = prompt("Do you want to play again?");
+                if(answer == "yes" || answer == "y"){
+                    guess = 0;
+                    playGame();
+                } else {
+            "Game Over!"
+        }}
  
     function playGame() {
-        userNumber = prompt("Enter a number between 1 and 5.");
+        userNumber = prompt("Enter a number between 1 and 5. ");
         if (userNumber > randomNumber) {    
             alert("Too high!");
             guess++;
-            if (guess >=4) { gameOver();}
+            alert("You have used " + guess + " guesses.");
+            if (guess >=4) {gameOver();}
             playGame();
     } 
         else if (userNumber < randomNumber) {
             alert("Too low!"); 
             guess++;
+            alert("You have used " + guess + " guesses.");
             if (guess >=4) { gameOver();}
             playGame();
     }
@@ -48,12 +51,17 @@
         if (userNumber > randomNumber) {    
             alert("Too high!");
             guess++;
+            alert("You have used " + guess + " guesses.");
+            if (guess >=4) {gameOver();}
+           
             round2();
             
     } 
         else if (userNumber < randomNumber) {
             alert("Too low!");  
             guess++;
+            alert("You have used " + guess + " guesses.");
+            if (guess >=4) {gameOver();}
             round2();
     }
         else {
@@ -65,10 +73,14 @@
             userNumber = prompt("Enter a number between 1 and 25.");
             if (userNumber > randomNumber) {    
                 alert("Too high!");
+                alert("You have used " + guess + " guesses.");
+                if (guess >=4) {gameOver();}
                 round3();
         } 
             else if (userNumber < randomNumber) {
-            alert("Too low!");  
+            alert("Too low!"); 
+            alert("You have used " + guess + " guesses.");
+            if (guess >=4) {gameOver();}
             round3();
         }
             else {
